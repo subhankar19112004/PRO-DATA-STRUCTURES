87074355abfbf662b14344208920af4dd50b0124
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2025-06-28 03:54:46 +0530

    improved bubble sort configuration

--- Sorting/bubbleSort.js
+++ Sorting/bubbleSort.js
@@ -3,14 +3,19 @@ function bubbleSort(arr) {
     let n = arr.length;
 
     for (let i = 0; i < n - 1; i++) {
+        let isSwapped = false;
         console.log(arr)
         for (let j = 0; j < n - 1 - i; j++) {
             if (arr[j] > arr[j + 1]) {
                 let temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
+                isSwapped = true;
                 console.log(arr)
             }
+        }
+        if (!isSwapped) {
+            break;
         }
     }
     return arr;
