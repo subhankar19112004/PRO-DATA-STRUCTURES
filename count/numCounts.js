72d27792345ea403ee4866131bf5f988eba8ded8
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-01-09 04:52:06 +0530

    reWrite number of counts in a digit

--- count/numCounts.js
+++ count/numCounts.js
@@ -1,13 +1,26 @@
-// Write a function that returns the count of digits fora in a a number.
-function numCounts(n) { 
-    if( n == 0)return 1;
-    n = Math.abs(n); // Handle negative numbers  
-    let count = 0;
-    while (n > 0) {
-        n = Math.floor(n / 10);
-        count++;
-    }
-    return count
-} 
+// // Write a function that returns the count of digits fora in a a number.
+// function numCounts(n) { 
+//     if( n == 0)return 1;
+//     n = Math.abs(n); // Handle negative numbers  
+//     let count = 0;
+//     while (n > 0) {
+//         n = Math.floor(n / 10);
+//         count++;
+//     }
+//     return count
+// } 
 
-console.log(numCounts(56245));
+// console.log(numCounts(56245));
+
+
+function numCounts (n){
+    if( n === 0) return 1;
+    n = Math.abs(n);
+    let count = 0;
+    while( n > 0 ){
+        n = Math.floor(n / 10);
+        count++
+    }
+    return count;
+} 
+console.log(numCounts(1234567890))
