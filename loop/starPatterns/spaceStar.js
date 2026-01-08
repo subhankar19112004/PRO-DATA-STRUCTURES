5f227077ba6b6233f1f36ec5fe39eda73afb4848
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-01-09 04:54:06 +0530

    all added

--- loop/starPatterns/spaceStar.js
+++ loop/starPatterns/spaceStar.js
@@ -1,4 +1,4 @@
-// let n = 5;
+let n = 5;
 // for( let i =0; i < n; i++){
 //     let row = "";
 //     for(let j = 0; j < n - (i+1);j++ ){
@@ -26,18 +26,44 @@
 // }
 
 
-let n = 5;
-let toggle = 1;
-for( let i = 0;  i< n; i++){
-    let row = "";
-    for( let j = 0; j < i+1; j++){
-        row = row  + toggle;
-        if( toggle == 1){
-            toggle = 0;
-        }else { 
-            toggle = 1;
-        }
-    }
-    console.log(row);
+// let n = 5;
+// let toggle = 1;
+// for( let i = 0;  i< n; i++){
+//     let row = "";
+//     for( let j = 0; j < i+1; j++){
+//         row = row  + toggle;
+//         if( toggle == 1){
+//             toggle = 0;
+//         }else { 
+//             toggle = 1;
+//         }
+//     }
+//     console.log(row);
     
+// }
+
+// for( let i = 0; i < 5; i++ ){
+//     let row = "";
+//     for(let k = 0; k < 5 - (i + 1 ); k++){
+//         row += " ";
+//     }
+//     for(let j = 0; j < i + 1; j++){
+//         row += "*";
+//     }
+//     console.log(row);
+// }
+
+for( let i = 0; i < n; i++ ){
+    let row = "";
+    let toggle = 1;
+    for ( let j =0; j < i + 1; j++ ){
+        row = row + toggle;
+        // if ( toggle == 1){
+        //     toggle = 0;
+        // }else {
+        //     toggle = 1;
+        // }
+        toggle = toggle === 1 ? 0 : 1;
+    }
+    console.log(row)
 }
--- loop/starPatterns/statSquare.js
+++ loop/starPatterns/statSquare.js
@@ -1,10 +1,19 @@
-let n = 4;
-for( let i = 0; i < n; i++){
-    let row = "";
-    for( let j = 0; j < n; j++){
-        row = row + "*  ";
+// let n = 4;
+// for( let i = 0; i < n; i++){
+//     let row = "";
+//     for( let j = 0; j < n; j++){
+//         row = row + "*  ";
 
+//     }
+//     console.log(row);
+   
+// }
+
+let n = 4;
+for ( let i = 0; i < n; i++ ){
+    let row = "";
+    for( let j = 0; j < n; j++ ){
+        row = row + "*  ";
     }
     console.log(row);
-   
 }
