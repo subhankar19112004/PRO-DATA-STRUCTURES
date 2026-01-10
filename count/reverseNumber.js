eaf6dca2b4c6550038c40b88aefcc0d88b3443e1
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-01-10 05:57:04 +0530

    refactor reverseNumber function for improved readability

--- count/reverseNumber.js
+++ count/reverseNumber.js
@@ -1,12 +1,14 @@
-function reverseNumber(n){
+function reverseNumber(n) {
     let nCopy = n;
     n = Math.abs(n);
     let rev = 0;
-    while( n > 0 ){
+    while (n > 0) {
         let rem = n % 10;
-        rev = ( 10 * rev ) + rem;
-        n = Math.floor( n/10 );
+        rev = (10 * rev) + rem;
+        n = Math.floor(n / 10);
     }
+    let limit = Math.pow(2, 31);
+    rev = rev > limit - 1 || rev < -limit ? 0 : rev;
     return nCopy < 0 ? -rev : rev;
 }
 console.log(reverseNumber(12345));
