d1fb49096e318c0d786a01402be1470d21906523
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-01-17 19:30:59 +0530

    remove element revised

--- arrays/removeElements.js
+++ arrays/removeElements.js
@@ -1,16 +1,29 @@
-function removeElement ( arr, val){
-    let x = 0;
-    for( let i = 0; i < arr.length; i++){
-        if(arr[i] != val){
-            arr[x] = arr[i];
-            console.log(arr, x, i)
-            x = x + 1;
-            console.log(arr, x, i)
+// function removeElement ( arr, val){
+//     let x = 0;
+//     for( let i = 0; i < arr.length; i++){
+//         if(arr[i] != val){
+//             arr[x] = arr[i];
+//             console.log(arr, x, i)
+//             x = x + 1;
+//             console.log(arr, x, i)
             
+//         }
+//     }
+//     return ( x );
+// }
+
+function removeElement(nums, val){
+    let k = 0;
+    for( let i = 0; i < nums.length; i++){
+        if(nums[i] !== val){
+             nums[k] = nums[i];
+             k++;
         }
     }
-    return ( x );
+    console.log(nums[k]);
+    return nums;
+
 }
 
-let arr = [ 3,2,2,3 ];
-console.log(removeElement(arr, 2));
+let nums = [ 3,2,2,3 ];
+console.log(removeElement(nums, 2));
